6e4323aa1cf8e8a597eec1e66c7d3ff65395806b
Ramachandiran007 <ramachandiran2001@gmail.com>
2021-07-08 20:59:32 +0000

    changes maded

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    if(principal == "" || principal <= 0)
+    {
+        //alert :if u not entered a value 
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+    //display  the result
+    document.getElementById("result").innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + year + "</span>";
 }
-        
+
+function updateRate()
+{
+    //updates the value of a slider
+    var rateval = document.getElementById("rate");
+    document.getElementById("rate_val").innerHTML = rateval.value; 
+}
